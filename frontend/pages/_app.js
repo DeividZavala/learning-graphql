0147c04a40d34c1352b909e17da77fc3e7ff6752
David <crusedmoss@gmail.com>
2022-09-14 11:47:33 -0500

    adding apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,15 +3,32 @@ import NProgress from "nprogress";
 import "../components/styles/nprogress.css";
 
 import Page from "../components/Page";
+import { ApolloProvider } from "@apollo/client";
+import withData from "../lib/withData";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // this exposes the query to the user
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(App);
